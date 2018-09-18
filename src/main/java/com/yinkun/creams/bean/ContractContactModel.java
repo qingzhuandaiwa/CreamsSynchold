3654726e62aaf502d338acbf1c6783e40afe4f7d
郭晶 <guojing@iworkgo.com>
2018-09-18 21:22:11 +0800

    添加合同的model实体映射类

---
+++ src/main/java/com/yinkun/creams/bean/ContractContactModel.java
@@ -0,0 +1,77 @@
+package com.yinkun.creams.bean;
+
+import java.math.BigDecimal;
+
+public class ContractContactModel {
+	
+	private String id;
+	
+	private String address;
+	
+	private String companyName;
+	
+	private String email;
+	
+	private BigDecimal name;
+
+	private String tel;
+
+	private String contractId;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public BigDecimal getName() {
+		return name;
+	}
+
+	public void setName(BigDecimal name) {
+		this.name = name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getContractId() {
+		return contractId;
+	}
+
+	public void setContractId(String contractId) {
+		this.contractId = contractId;
+	}
+
+}
---
+++ src/main/java/com/yinkun/creams/bean/ContractRoomModel.java
@@ -0,0 +1,77 @@
+package com.yinkun.creams.bean;
+
+import java.math.BigDecimal;
+
+public class ContractRoomModel {
+	
+	private String id;
+	
+	private String roomId;
+	
+	private String roomNumber;
+	
+	private String floor;
+	
+	private BigDecimal areaSize;
+	
+	private BigDecimal price;
+	
+	private String contractId;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRoomId() {
+		return roomId;
+	}
+
+	public void setRoomId(String roomId) {
+		this.roomId = roomId;
+	}
+
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public String getFloor() {
+		return floor;
+	}
+
+	public void setFloor(String floor) {
+		this.floor = floor;
+	}
+
+	public BigDecimal getAreaSize() {
+		return areaSize;
+	}
+
+	public void setAreaSize(BigDecimal areaSize) {
+		this.areaSize = areaSize;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getContractId() {
+		return contractId;
+	}
+
+	public void setContractId(String contractId) {
+		this.contractId = contractId;
+	}
+	
+}
